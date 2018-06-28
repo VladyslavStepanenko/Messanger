9519fb5c4d6333738814db675e2bfb46ad43a593
Vlad Stepanenko <vladyslav.stepanenko@gmail.com>
2018-06-28 17:08:03 +0300

    Add User domain entity

---
+++ Messanger.Domain/User.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Messanger.Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AvatarUrl { get; set; }
+
+        public ICollection<Dialog> Dialogs { get; set; }
+
+        public User()
+        {
+            Dialogs = new HashSet<Dialog>();
+        }
+    }
+}
