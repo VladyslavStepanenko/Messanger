01ee3761cdc44805f8de71d292e05880ee969c85
Vlad Stepanenko <vladyslav.stepanenko@gmail.com>
2018-06-28 19:00:37 +0300

    Add UsersController

---
+++ Messanger.Api/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using Messanger.Domain;
+using Messanger.Infra.DataContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Messanger.Api.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private MessangerDbContext db;
+
+        public UsersController()
+        {
+            db = new MessangerDbContext();
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return db.Users;
+        }
+    }
+}
---
+++ Messanger.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Messanger.Api.WebApiApplication" Language="C#" %>
