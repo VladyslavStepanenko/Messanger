17a0e1b980132f3fa23148860221f4674c28682e
Vlad Stepanenko <vladyslav.stepanenko@gmail.com>
2018-06-28 17:16:27 +0300

    Add Message domain entity

---
+++ Messanger.Domain/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace Messanger.Domain
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+
+        public int DialogId { get; set; }
+        public virtual Dialog Dialog { get; set; }
+    }
+}
