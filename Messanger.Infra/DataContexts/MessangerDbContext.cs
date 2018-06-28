91c5d30c80d2a2e338e71aeb8e6f920ae9d26dd4
Vlad Stepanenko <vladyslav.stepanenko@gmail.com>
2018-06-28 17:35:34 +0300

    Add messanger data context for entity framework

---
+++ Messanger.Infra/DataContexts/MessangerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Messanger.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messanger.Infra.DataContexts
+{
+    public class MessangerDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Dialog> Dialogs { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        public MessangerDbContext() : base("MessangerDb")
+        {
+
+        }
+    }
+}
