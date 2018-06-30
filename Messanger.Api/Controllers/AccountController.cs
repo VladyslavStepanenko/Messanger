1f7307ed1d4da21bce63323dd692ee2f77213b58
Vlad Stepanenko <vladyslav.stepanenko@gmail.com>
2018-06-30 15:25:36 +0300

    Add Account controller and implement get user profile method

---
+++ Messanger.Api/Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Messanger.Api.Models;
+using Messanger.Infra.DataContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Messanger.Api.Controllers
+{
+    [RoutePrefix("api/account")]
+    public class AccountController : ApiController
+    {
+        private MessangerDbContext dbContext;
+
+        public AccountController()
+        {
+            dbContext = new MessangerDbContext();
+        }
+
+        [Route("{id:int}")]
+        [Authorize]
+        public HttpResponseMessage GetUser(int id)
+        {
+            var user = dbContext.Users.Find(id);
+            HttpResponseMessage response = new HttpResponseMessage();
+            if(user == null)
+            {
+                response = Request.CreateErrorResponse(HttpStatusCode.NotFound, $"User with id = {id} not found");
+                return response;
+            }
+            response = Request.CreateResponse(HttpStatusCode.OK, new
+            {
+                id = user.Id,
+                name = user.Name,
+                password = user.Password,
+                avatarUrl = user.AvatarUrl
+            });
+            return response;
+        }
+
+    }
+}
